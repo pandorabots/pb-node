330fb69b7a2647462632923e363ff6c5cb65d3ea
djf <djfdev@gmail.com>
2014-07-23 15:27:48 -0700

    restructure

---
+++ pandorabots.js
@@ -0,0 +1,75 @@
+var http = require('http');
+var fs = require('fs');
+var request = require('request');
+var querystring = require('querystring');
+
+module.exports = {
+
+  createBot: function(options, callback) {
+    var path = '/bot/' + options.username + '/' + options.botname + '?';
+    var qs = querystring.stringify(options.q);
+    var params = {
+      url: 'https://' + options.host + path + qs
+    };
+    console.log(params.url);
+    request.put(params, function(error, response, body) {
+      callback(error, response, body);
+    });
+  },
+
+  deleteBot: function(options, callback) {
+    var path = '/bot/' + options.username + '/' + options.botname + '?';
+    var qs = querystring.stringify(options.q);
+    var params = {
+      url: 'https://' + options.host + path + qs
+    };
+    request.del(params, function(error, response, body) {
+      callback(error, response, body);
+    });
+  },
+
+  uploadFile: function(options, callback) {
+    if (options.filename) {
+      var path = '/bot/' + options.username + '/' + options.botname + '/' + options.filetype + '/' + options.filename + '?';
+    } else {
+      var path = '/bot/' + options.username + '/' + options.botname + '/' + options.filetype + '?';
+    }
+    var qs = querystring.stringify(options.q);
+    var params = {
+      url: 'https://' + options.host + path + qs
+    }
+    fs.stat(options.filepath, function(error, stat) {
+      if (error) { throw error; }
+      params.headers = { "Content-Length": stat.size }
+      fs.createReadStream(options.filepath)
+        .pipe(request.put(params, function(err, response, body) {
+          callback(error, response, body);
+        }));
+    });
+  },
+
+  compileBot: function(options, callback) {
+    var path = '/bot/' + options.username + '/' + options.botname + '/verify?';
+    var qs = querystring.stringify(options.q);
+    var params = {
+      url: 'https://' + options.host + path + qs,
+    };
+    request.get(params, function(error, response, body) {
+      callback(error, response, body);
+    });
+  },
+
+  talk: function(options, callback) {
+    var path = '/talk/' + options.username + '/' + options.botname + '?';
+    var qs = querystring.stringify(options.q);
+    var params = {
+      url: 'https://' + options.host + path + qs,
+    };
+    request.post(params, function(error, response, body) {
+      callback(error, response, body);
+    });
+  },
+
+};
+
+
